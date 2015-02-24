1aab0ab1c5305e0ba02f8697fafb735a70505d86
vtzika <varvara.tzika@live.com>
2015-02-24 16:51:18 +0100

    In case of an error it logs it but it doesn't stop gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -4,27 +4,32 @@ var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
 	sass = require('gulp-ruby-sass');
 
-
 // Scripts Tasks
 // Uglifies the js files
 gulp.task('scripts', function(){
 	gulp.src('application/js/*.js')
-	.pipe(uglify())
-	.pipe(gulp.dest('build/js'));
+		.pipe(uglify())
+		.on('error', function(err) {
+			console.error('Error!', err.message);
+		})
+		.pipe(gulp.dest('build/js'));
 });
 
 // Styles Tasks
 // 
 gulp.task('styles', function(){
 	return sass('./application/scss/', { style: 'compressed' })
+	.on('error', function(err) {
+		console.error('Error!', err.message);
+	})
 	.pipe(gulp.dest('css/'))
 });
 
 // Watch Tasks
 // Watches js files
 gulp.task('watch', function(){
+	gulp.watch('scss/*', ['styles']);
 	gulp.watch('application/**/*.js', ['scripts']);
-	gulp.watch('scss/**/*.scss', ['styles']);
 });
 
 gulp.task('default', ['scripts', 'styles', 'watch']); 

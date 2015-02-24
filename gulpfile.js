fd23b5bb77588fe91540e0e95cd14745dcb9f935
vtzika <varvara.tzika@live.com>
2015-02-24 16:15:36 +0100

    task to watch changes on the sass files, compress the files and compile them to css is added

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 // It will import the node packages
 // and it will assign each one in a variable
 var gulp = require('gulp'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+	sass = require('gulp-ruby-sass');
 
 
 // Scripts Tasks
-// Uglifies
+// Uglifies the js files
 gulp.task('scripts', function(){
 	gulp.src('application/js/*.js')
 	.pipe(uglify())
@@ -15,13 +16,15 @@ gulp.task('scripts', function(){
 // Styles Tasks
 // 
 gulp.task('styles', function(){
-	console.log('run styles')
+	return sass('./application/scss/', { style: 'compressed' })
+	.pipe(gulp.dest('css/'))
 });
 
 // Watch Tasks
 // Watches js files
 gulp.task('watch', function(){
 	gulp.watch('application/**/*.js', ['scripts']);
+	gulp.watch('scss/**/*.scss', ['styles']);
 });
 
 gulp.task('default', ['scripts', 'styles', 'watch']); 
